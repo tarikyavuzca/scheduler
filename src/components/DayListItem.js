6023e14b910a950785505c763015ce627a457ad2
tarikyavuzca <tarikyavuzca@gmail.com>
2021-10-27 18:44:50 -0400

    refactoring the DayList Application, DaylistItem and story/index files

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -21,9 +21,14 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots()}</h3>
+    <li
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      selected={props.selected}
+    >
+      <h2 className="text--regular">{props.value}</h2>
+      {/* <h3 className="text--light">{formatSpots()}</h3> */}
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -61,13 +61,13 @@ storiesOf("DayList", module)
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
   })
   .add("Monday", () => (
-    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Monday"} onChange={action("setDay")} />
   ))
   .add("Tuesday", () => (
-    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
   ))
   .add("Wednesday", () => (
-    <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
   ));
 
 const interviewer = {
@@ -121,8 +121,9 @@ storiesOf("InterviewerList", module)
     <InterviewerList interviewers={interviewers} interviewer={3} />
   ))
   .add("Clickable", () => (
-    <InterviewerList
-      interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+    <InterviewerListItem
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={() => action("setInterviewer")(interviewer.id)}
     />
   ));
