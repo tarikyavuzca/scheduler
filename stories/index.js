863536a4095c88714f2eaa86e3ed2af54e0a3ee2
tarikyavuzca <tarikyavuzca@gmail.com>
2021-10-27 23:42:02 -0400

    Form component created and updated

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -157,7 +158,24 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   ))
-  .add("Status", () => <Status message="Deleting" />)
+  .add("Status Delete", () => <Status message="Deleting" />)
+  .add("Status Save", () => <Status message="Saving" />)
   .add("Error", () => (
     <Error message="Could not delete appointment" onClose={action("onClose")} />
+  ))
+  .add("Form Edit", () => (
+    <Form
+      student="YTD"
+      interviewer={3}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Form Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
   ));
