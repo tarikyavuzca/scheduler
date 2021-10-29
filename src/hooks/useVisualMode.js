1c115a859f913e75b8f26deaaa68f06d4464982d
tarikyavuzca <tarikyavuzca@gmail.com>
2021-10-29 17:43:49 -0400

    building the hooks and passed the test cases

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    setMode(newMode);
+
+    setHistory((history) => {
+      if (replace) {
+        const newHistory = [...history];
+        newHistory.splice(-1, 1, newMode);
+        return newHistory;
+      } else {
+        return [...history, newMode];
+      }
+    });
+  };
+
+  const back = () => {
+    setHistory((history) => {
+      const newHistory = [...history].slice(0, -1);
+      setMode(newHistory[newHistory.length - 1]);
+      return newHistory;
+    });
+  };
+  return { mode, transition, back };
+}
