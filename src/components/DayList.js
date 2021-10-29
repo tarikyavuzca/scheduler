3e4955574d73df1c311bc3f0d04f6f6023282f37
tarikyavuzca <tarikyavuzca@gmail.com>
2021-10-28 21:20:21 -0400

    Daylist and DayListItem fupdated

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,16 +2,17 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const days = props.days.map((day) => {
-    return (
-      <DayListItem
-        key={day.id}
-        name={day.name}
-        spots={day.spots}
-        selected={day.name === props.value}
-        setDay={props.onChange}
-      />
-    );
-  });
-  return days;
+  return (
+    <ul>
+      {props.days.map((day) => (
+        <DayListItem
+          key={day.id}
+          name={day.name}
+          spots={day.spots}
+          selected={day.name === props.value}
+          setDay={() => props.setDay(day.name)}
+        />
+      ))}
+    </ul>
+  );
 }
