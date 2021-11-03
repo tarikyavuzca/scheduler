0a103c00eea169ffabd3869573caa0b9be13b488
tarikyavuzca <tarikyavuzca@gmail.com>
2021-11-03 14:43:19 -0400

    Form.test file updated with the given information, appointment.test updated

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -7,7 +7,7 @@ import React from "react";
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
 */
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 
 /*
   We import the component that we are testing
@@ -17,6 +17,9 @@ import Appointment from "components/Appointment/index";
 /*
   A test that renders a React Component
 */
+
+afterEach(cleanup);
+
 describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
